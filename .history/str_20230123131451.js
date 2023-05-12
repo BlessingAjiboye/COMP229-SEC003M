c058379291357e5d1ade7ef21d384577d9a5dfdd
Blessing Ajiboye <blessjiboye@yahoo.com>
2023-05-12 10:40:11 -0400

    first commit

---
+++ .history/str_20230123131451.js
@@ -0,0 +1,6 @@
+var  myString     =     "    WEB 222   "; 
+console.log( myString.length );    // 14
+
+myString = myString.trim();
+console.log( myString );           // WEB 222
+console.log( myString.length );    // 7
